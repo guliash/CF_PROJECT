b1d2781291a73400cd6140a6a04c5403b01b3976
Artem Gilmudinov <guliash92@gmail.com>
2015-07-11 13:08:46 +0300

    pair;

---
+++ com/github/guliash/structures/PairInt.java
@@ -0,0 +1,16 @@
+package com.github.guliash.structures;
+
+/**
+ * Created by gulash on 11.07.15.
+ */
+public class PairInt {
+    public int x, y;
+    public PairInt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return x + " " + y;
+    }
+}
---
+++ com/github/guliash/structures/PairLong.java
@@ -0,0 +1,17 @@
+package com.github.guliash.structures;
+
+/**
+ * Created by gulash on 11.07.15.
+ */
+public class PairLong {
+    public long x, y;
+    public PairLong(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return x + " " + y;
+    }
+
+}
